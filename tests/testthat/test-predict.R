46fed69a04d000712a70565c87c6462d7edf40b5
Ayla Pearson <ayla@poissonconsulting.ca>
2026-09-07 09:07:46 -0700

    Update tests/testthat/test-predict.R
    Co-authored-by: Rebecca Fisher <r.fisher@aims.gov.au>

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -30,7 +30,7 @@ test_that("predict.bayesmanecfit returns summary by default", {
   )
 })
 
-# EXPECTED-CHANGE predict method moves to bayesnec (test will be removed)
+# EXPECTED-CHANGE predict method moves to bayesnec (test will be removed) #39
 test_that("predict.bayesmanecfit summary = FALSE returns raw posterior", {
   preds <- predict(bayesnec::manec_example, summary = FALSE)
 
